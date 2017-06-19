63d868ac9cefcbfc7e9b685b67424ed29805615a
Adi Andrea <dekz.itfz@gmail.com>
2017-06-19 19:00:46 +0800

    add click listener to listview

--- app/src/main/java/id/dekz/mylayoutapp/DashboardActivity.java
+++ app/src/main/java/id/dekz/mylayoutapp/DashboardActivity.java
@@ -2,8 +2,11 @@ package id.dekz.mylayoutapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by DEKZ on 6/14/2017.
@@ -21,8 +24,17 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         listview = (ListView) findViewById(R.id.listview);
-        strings = new String[]{"Jeruk", "Apel", "Mangga", "Manggis", "Pisang"};
+        strings = new String[]{"Jeruk", "Apel", "Mangga", "Manggis", "Pisang",
+                "Jeruk", "Apel", "Mangga", "Manggis",
+                "Pisang", "Jeruk", "Apel", "Mangga", "Manggis", "Pisang"};
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strings);
         listview.setAdapter(arrayAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(DashboardActivity.this, strings[i], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
