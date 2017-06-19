11e737e64c0ad56284d32e9794dc84a263ae33dc
Adi Andrea <dekz.itfz@gmail.com>
2017-06-19 18:53:11 +0800

    buat simple listview

--- app/src/main/java/id/dekz/mylayoutapp/DashboardActivity.java
+++ app/src/main/java/id/dekz/mylayoutapp/DashboardActivity.java
@@ -2,6 +2,8 @@ package id.dekz.mylayoutapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by DEKZ on 6/14/2017.
@@ -9,9 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private ListView listview;
+    private String[] strings;
+    private ArrayAdapter arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        listview = (ListView) findViewById(R.id.listview);
+        strings = new String[]{"Jeruk", "Apel", "Mangga", "Manggis", "Pisang"};
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strings);
+        listview.setAdapter(arrayAdapter);
     }
 }
