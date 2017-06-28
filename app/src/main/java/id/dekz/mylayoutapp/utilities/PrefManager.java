92f57f191bc0d246e269f751417b4ef35f544aab
Adi Andrea <dekz.itfz@gmail.com>
2017-06-28 19:37:18 +0800

    add class PrefManager

---
+++ app/src/main/java/id/dekz/mylayoutapp/utilities/PrefManager.java
@@ -0,0 +1,41 @@
+package id.dekz.mylayoutapp.utilities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by DEKZ on 6/28/2017.
+ */
+
+public class PrefManager {
+
+    public PrefManager() {
+
+    }
+
+    public void saveString(Context context, String key, String value){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(key, value)
+                .apply();
+    }
+
+    public String getString(Context context, String key){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(key, null);
+    }
+
+    public void saveBool(Context context, String key, Boolean value){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(key, value)
+                .apply();
+    }
+
+    public boolean getBool(Context context, String key){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(key, false);
+    }
+
+}
